74f3f9888614273cce81881831e1a0c850cc03ec
Amir Haghighi <hagh73h@gmail.com>
2023-08-18 10:05:38 +0330

    nothing special

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Home Page</div>;
+  return (
+    <div className="App">
+      <header>header</header>
+      <nav>
+        <img src="" alt="logo" />
+        <li>home</li>
+        <li>about</li>
+        <li>more</li>
+      </nav>
+      <main>main</main>
+      <footer>footer</footer>
+    </div>
+  );
 }
 
 export default App;
